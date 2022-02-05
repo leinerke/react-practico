b84ed47ce067e47f587afe6e1239069d75033cbc
Leinerke Suarez <leinerke2000@gmail.com>
2022-02-05 16:29:46 -0500

    Custom Hooks para la tienda.

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import '@styles/ProductItem.scss';
 import addToCart from '@icons/bt_add_to_cart.svg';
 
-const ProductItem = () => {
+const ProductItem = ({ product: { price, images, title } }) => {
 	const [cart, setCart] = useState([]);
 
 	const handleClick = () => {
@@ -11,11 +11,11 @@ const ProductItem = () => {
 
 	return (
 		<div className="ProductItem">
-			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+			<img src={images[0]} alt={title} />
 			<div className="product-info">
 				<div>
-					<p>$120,00</p>
-					<p>Bike</p>
+					<p>{price}</p>
+					<p>{title}</p>
 				</div>
 				<figure>
 					<img src={addToCart} alt="add to cart" />
