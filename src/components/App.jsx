3f4aebc6091109dc32a8549b93f8fe040d86e982
Leinerke Suarez <leinerke2000@gmail.com>
2022-02-05 10:50:55 -0500

    Estilos en los componentes de React.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import '../styles/global.scss';
+import { Layout } from '../containers/Layout';
+import { Login } from '../containers/Login';
 
 const App = () => {
   return (
-    <h1>Hola mundo!!!!</h1>
+    <Layout>
+      <Login />
+    </Layout>
   );
 };
 
