9ef12ef098ffd66aa014976bc3744f30fd3ba6b6
Leinerke Suarez <leinerke2000@gmail.com>
2022-02-05 13:54:30 -0500

    Solución al reto: componentes y rutas de React Shop.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { App } from './routes/App';
+import App from './routes/App';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('app'));
